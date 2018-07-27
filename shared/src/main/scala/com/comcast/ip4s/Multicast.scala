ca796fba4676b0369ab70a46f181ad8fd442f9c7
mpilquist <michael_pilquist@comcast.com>
2018-07-26 21:18:32 -0400

    Changed strategy so instead of cooperative equality, a request to create a multicast address for a source specific ip results in a SSM

--- shared/src/main/scala/com/comcast/ip4s/Multicast.scala
+++ shared/src/main/scala/com/comcast/ip4s/Multicast.scala
@@ -30,16 +30,14 @@ sealed trait Multicast[+A <: IpAddress] extends Product with Serializable {
 
 object Multicast {
   private case class DefaultMulticast[+A <: IpAddress](address: A) extends Multicast[A] {
-    override def equals(that: Any): Boolean = that match {
-      case that: Multicast[A] => address == that.address
-    }
-    override def hashCode: Int = address.hashCode
     override def toString: String = address.toString
   }
 
   /** Constructs a multicast IP address. Returns `None` is the supplied address is not in the valid multicast range. */
   def apply[A <: IpAddress](address: A): Option[Multicast[A]] =
-    if (address.isMulticast) Some(new DefaultMulticast(address)) else None
+    if (address.isSourceSpecificMulticast) Some(SourceSpecificMulticast.unsafeCreate(address))
+    else if (address.isMulticast) Some(DefaultMulticast(address))
+    else None
 
   implicit def ordering[A <: IpAddress]: Ordering[Multicast[A]] = Ordering.by(_.address)
 
@@ -60,16 +58,15 @@ sealed trait SourceSpecificMulticast[+A <: IpAddress] extends Multicast[A] {
 
 object SourceSpecificMulticast {
   private case class DefaultSourceSpecificMulticast[+A <: IpAddress](address: A) extends SourceSpecificMulticast[A] {
-    override def equals(that: Any): Boolean = that match {
-      case that: Multicast[A] => address == that.address
-    }
-    override def hashCode: Int = address.hashCode
     override def toString: String = address.toString
   }
 
   /** Constructs a source specific multicast IP address. Returns `None` is the supplied address is not in the valid source specific multicast range. */
   def apply[A <: IpAddress](address: A): Option[SourceSpecificMulticast[A]] =
     if (address.isSourceSpecificMulticast) Some(new DefaultSourceSpecificMulticast(address)) else None
+
+  private[ip4s] def unsafeCreate[A <: IpAddress](address: A): SourceSpecificMulticast[A] =
+    DefaultSourceSpecificMulticast(address)
 
   implicit def ordering[A <: IpAddress]: Ordering[SourceSpecificMulticast[A]] =
     new Ordering[SourceSpecificMulticast[A]] {
