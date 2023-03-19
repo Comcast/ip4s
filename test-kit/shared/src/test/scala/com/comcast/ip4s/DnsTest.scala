660e2f3ac1a9df6abd7552b8e8d02294c5cd7fba
Arman Bilge <armanbilge@gmail.com>
2023-03-19 16:56:48 +0000

    Fix assertions

--- test-kit/shared/src/test/scala/com/comcast/ip4s/DnsTest.scala
+++ test-kit/shared/src/test/scala/com/comcast/ip4s/DnsTest.scala
@@ -45,16 +45,17 @@ class DnsTest extends CatsEffectSuite {
   test("loopback") {
     val loopbacks = Set(ip"127.0.0.1", ip"::1")
     Dns[IO].loopback.flatMap { loopback =>
-      IO(loopbacks.contains(clue(loopback)))
+      IO(assert(loopbacks.contains(clue(loopback))))
     }
-    assertIO(Dns[IO].loopback.map(_.some), IpAddress.fromString("127.0.0.1"))
   }
 
   test("resolve unknown host") {
-    Dns[IO].resolve(host"not.example.com").void.recover { case ex: UnknownHostException =>
-      assert(
-        ex.getMessage == "not.example.com: Name or service not known" || ex.getMessage == "not.example.com: nodename nor servname provided, or not known"
-      )
+    (Dns[IO].resolve(host"not.example.com") >>
+      IO.raiseError(new AssertionError("Did not raise `UnknownHostException`"))).recover {
+      case ex: UnknownHostException =>
+        assert(
+          ex.getMessage == "not.example.com: Name or service not known" || ex.getMessage == "not.example.com: nodename nor servname provided, or not known"
+        )
     }
   }
 
