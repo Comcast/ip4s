14bc4194e9750fe6a14e8ba2e0e956488e92e1e3
Michael Pilquist <mpilquist@gmail.com>
2025-12-21 09:33:10 -0500

    Fix test

--- test-kit/shared/src/test/scala/com/comcast/ip4s/DnsTest.scala
+++ test-kit/shared/src/test/scala/com/comcast/ip4s/DnsTest.scala
@@ -53,10 +53,13 @@ class DnsTest extends CatsEffectSuite {
     (Dns[IO].resolve(host"not.example.com") >>
       IO.raiseError(new AssertionError("Did not raise `UnknownHostException`"))).recover {
       case ex: UnknownHostException =>
-        ex.printStackTrace()
-        assert(
-          ex.getMessage == "not.example.com: Name or service not known" || ex.getMessage == "not.example.com: nodename nor servname provided, or not known"
+        val msg = ex.getMessage
+        val expected = List(
+          "not.example.com: Name or service not known",
+          "not.example.com: nodename nor servname provided, or not known",
+          "not.example.com: No address associated with hostname"
         )
+        assert(expected.contains(msg), msg)
     }
   }
 
